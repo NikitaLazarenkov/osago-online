ac184fccdef2eeb4b44d56551c1cf255f8b71bbb
/Nikita L <nik.boost@gmail.com>
2018-06-02 15:49:55 +0300

    Add bottom navigation buttons
    Add bottom navigation buttons. Config slider in top navigation.

--- js/osago_calc.js
+++ js/osago_calc.js
@@ -11,4 +11,115 @@ jQuery(document).ready(function($) {
         $('#osago_calc').show();
         $('#kasko_calc').hide();
     });
+
+    $('#step1_go1, #step1_go2').click(function () {
+        $('#nav_position_1').removeClass('active');
+        $('#nav_position_2').addClass('active');
+        $('#osago_step_1_nav').hide();
+        $('#osago_step_2_nav').show();
+    });
+
+    $('#step2_back').click(function () {
+        $('#nav_position_2').removeClass('active');
+        $('#nav_position_1').addClass('active');
+        $('#osago_step_2_nav').hide();
+        $('#osago_step_1_nav').show();
+    });
+    $('#step2_go1, #step2_go2').click(function () {
+        $('#nav_position_2').removeClass('active');
+        $('#nav_position_3').addClass('active');
+        $('#osago_step_2_nav').hide();
+        $('#osago_step_3_nav').show();
+    });
+
+    $('#step3_back').click(function () {
+        $('#nav_position_3').removeClass('active');
+        $('#nav_position_2').addClass('active');
+        $('#osago_step_3_nav').hide();
+        $('#osago_step_2_nav').show();
+    });
+    $('#step3_go1, #step3_go2').click(function () {
+        $('#nav_position_3').removeClass('active');
+        $('#nav_position_4').addClass('active');
+        $('#osago_step_3_nav').hide();
+        $('#osago_step_4_nav').show();
+    });
+
+    $('#step4_back').click(function () {
+        $('#nav_position_4').removeClass('active');
+        $('#nav_position_3').addClass('active');
+        $('#osago_step_4_nav').hide();
+        $('#osago_step_3_nav').show();
+    });
+    $('#step4_go1, #step4_go2').click(function () {
+        $('#nav_position_4').removeClass('active');
+        $('#nav_position_5').addClass('active');
+        $('#osago_step_4_nav').hide();
+        $('#osago_step_5_nav').show();
+    });
+
+    $('#step5_back').click(function () {
+        $('#nav_position_5').removeClass('active');
+        $('#nav_position_4').addClass('active');
+        $('#osago_step_5_nav').hide();
+        $('#osago_step_4_nav').show();
+    });
+
+    /*$('.check_position').click(function () {
+
+        if ($('.nav_position_1').hasClass('active')) {
+            $('#osago_step_1_nav').show();
+            $('#osago_step_2_nav').hide();
+            $('#osago_step_3_nav').hide();
+            $('#osago_step_4_nav').hide();
+            $('#osago_step_5_nav').hide();
+        } else {
+            $('#osago_step_1_nav').hide();
+        }
+
+        if ($('.nav_position_2').hasClass('active')) {
+            $('#osago_step_2_nav').show();
+            $('#osago_step_1_nav').hide();
+            $('#osago_step_3_nav').hide();
+            $('#osago_step_4_nav').hide();
+            $('#osago_step_5_nav').hide();
+        } else {
+            $('#osago_step_2_nav').hide();
+        }
+
+        if ($('.nav_position_3').hasClass('active')) {
+            $('#osago_step_3_nav').show();
+            $('#osago_step_1_nav').hide();
+            $('#osago_step_2_nav').hide();
+            $('#osago_step_4_nav').hide();
+            $('#osago_step_5_nav').hide();
+        } else {
+            $('#osago_step_3_nav').hide();
+        }
+
+        if ($('.nav_position_4').hasClass('active')) {
+            $('#osago_step_4_nav').show();
+            $('#osago_step_1_nav').hide();
+            $('#osago_step_2_nav').hide();
+            $('#osago_step_3_nav').hide();
+            $('#osago_step_5_nav').hide();
+        } else {
+            $('#osago_step_4_nav').hide();
+        }
+
+        if ($('.nav_position_5').hasClass('active')) {
+            $('#osago_step_5_nav').show();
+            $('#osago_step_1_nav').hide();
+            $('#osago_step_2_nav').hide();
+            $('#osago_step_3_nav').hide();
+            $('#osago_step_4_nav').hide();
+        } else {
+            $('#osago_step_5_nav').hide();
+        }
+    });*/
+
+    
+
+
+    
 });
